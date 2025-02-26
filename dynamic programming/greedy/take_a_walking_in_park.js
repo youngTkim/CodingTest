6ebbd271daebd0dd9f3615800f98cded072908c5
김영택 <kooduck168@gmail.com>
2025-02-26 13:41:59 +0900

    take_a_walking_in_park.js

--- dynamic programming/greedy/take_a_walking_in_park.js
+++ dynamic programming/greedy/take_a_walking_in_park.js
@@ -1,3 +1,5 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/172928
+
 function solution(park, routes) {
   let row = park.findIndex((r) => r.includes("S")),
     col = park[row].indexOf("S");
